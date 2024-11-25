686453a3f32057ce9601ad14676af1cd945b2538
TunjiTofu <adetunjitofunmi@gmail.com>
2024-11-25 05:09:47 +0100

    done with post module

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,14 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { forwardRef, Inject, Injectable } from "@nestjs/common";
 import { GetUsersParamDto } from "../DTOs/GetUsersParam.dto";
+import { AuthService } from "src/auth/providers/auth.service";
 
 @Injectable()
 export class UsersService {
+
+    constructor(
+        @Inject(forwardRef(() => AuthService))
+        private readonly authService: AuthService
+    ){}
 
     public findAll(
         getUserParamDto: GetUsersParamDto,
         limit: number,
         page: number
     ){
+        const isAuth = this.authService.isAuth();
+        console.log(isAuth);
+        
         return [
             {
                 firstName: 'john',
@@ -24,7 +33,7 @@ export class UsersService {
     /**
      * findUserById
      */
-    public findOneById(id: number) {
+    public findOneById(id: string) {
         return {
             id: 2,
             firstName: 'Tofunmi',
